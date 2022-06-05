a1f72325ac9aaa03239e90c7cb1e19e379266720
deepen77 <przemek.starostka@yahoo.com>
2022-06-05 20:22:40 +0200

    map function example01

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { array } from './data';
 import './App.css';
+import { useState } from 'react';
+
+
 
 function App() {
+
+  const [db, setDb] = useState(array)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {db.map((elm) => {
+        return (
+          <div key={elm.id}>
+            <h2>Name: {elm.name}</h2>
+            {elm.books.map((book) => {
+              return (
+                <div key={book.id}>
+                  <span>Book id: {book.id}</span>
+                  <span> | </span>
+                  <span>Book title: {book.title}</span>
+                  <span> | </span>
+                  <span>Book author: {book.author}</span>
+                </div>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,69 @@
+export let array = [
+  {
+    id: 1,
+    name: "Przemek",
+    age: 35,
+    books: [
+      { id: 1, title: "title1", author: "author1" },
+      { id: 2, title: "title2", author: "author2" },
+      { id: 3, title: "title3", author: "author3" },
+      { id: 4, title: "title4", author: "author4" },
+      { id: 5, title: "title5", author: "author5" },
+      { id: 6, title: "title6", author: "author6" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Alan",
+    age: 30,
+    books: [
+      { id: 7, title: "title7", author: "author7" },
+      { id: 8, title: "title8", author: "author8" },
+      { id: 9, title: "title9", author: "author9" },
+      { id: 10, title: "title10", author: "author10" },
+      { id: 11, title: "title11", author: "author11" },
+      { id: 12, title: "title12", author: "author12" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Bill",
+    age: 25,
+    books: [
+      { id: 13, title: "title13", author: "author13" },
+      { id: 14, title: "title14", author: "author14" },
+      { id: 15, title: "title15", author: "author15" },
+      { id: 16, title: "title16", author: "author16" },
+      { id: 17, title: "title17", author: "author17" },
+      { id: 18, title: "title18", author: "author18" },
+    ],
+  },
+  {
+    id: 4,
+    name: "John",
+    age: 18,
+    books: [
+      { id: 19, title: "title19", author: "author19" },
+      { id: 20, title: "title19", author: "author19" },
+      { id: 21, title: "title20", author: "author20" },
+      { id: 22, title: "title21", author: "author21" },
+      { id: 23, title: "title22", author: "author22" },
+      { id: 24, title: "title23", author: "author23" },
+    ],
+  },
+  {
+    id: 5,
+    name: "Will",
+    age: 21,
+    books: [
+      { id: 25, title: "title25", author: "author25" },
+      { id: 26, title: "title26", author: "author26" },
+      { id: 27, title: "title27", author: "author27" },
+      { id: 28, title: "title28", author: "author28" },
+      { id: 29, title: "title29", author: "author29" },
+      { id: 30, title: "title30", author: "author30" },
+    ],
+  },
+];
+
+
